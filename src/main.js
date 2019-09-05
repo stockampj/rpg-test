95005913c8adb847f176a0171bc482b792286b63
Jennifer Batara, Joel Stockamp, Kira Roberts and Molly Stambaugh <kira@>
2019-09-05 13:32:40 -0700

    Add Loot, Character, and Round Classes
    Also updated eslinter config
    Add new character for the player

--- src/main.js
+++ src/main.js
@@ -1,8 +1,18 @@
-// import $ from 'jquery';
+import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles.css';
+import {Round, Character, Loot} from './rpg';
 
+$(document).ready(function(){
+  const starterWeapon = new Loot('Fist','attack',0);
+  const starterArmor = new Loot('Moldy Clothes','hp',0);
+  let zombie = new Character('Betty the Brain Eater', 1, 10,3,starterWeapon,starterArmor,0);
+    zombie.selectCharacter();
+  let fightRound = new Round(zombie);
 
+  console.log(zombie,jock);
+  console.log(fightRound);
 
+});
 // var myGame = new Game
--- src/rpg.js
+++ src/rpg.js
@@ -1,39 +1,98 @@
-export function Game() {
-  this.row1 = [5, 3, "", "", 7, "", "", "", ""];
+// export class Game {
+//   constructor() {
+//
+//   }
+//
+// }
 
-}
-
-
-Game.prototype.numbOnly = function() {
-  var failureLog = [];
-  var allowedValues = [1, 2, 3, 4, 5, 6, 7, 8, 9, ""];
-
-  for (var i = 0; i < 9; i++) {
-    if (allowedValues.indexOf(this.row1[i]) < 0) {
-      failureLog.push([i]);
-    }
+export class Round {
+  let jock = new Character('Chad Michael Michaelson', 1, 2, 2, new Loot('Chad\'s Fist', 'attack', 3), new Loot('Jock Strap', 'hp', 1), 1);
+  let avClub = new Character('AV Club', 1, 2, 2 new Loot('16 mm Reel of Legend', 'attack', 1), new Loot('Ratty Hipster Sweatshirt', 'hp', 0), 3);
+  let cheer = new Character('Marsha', 1, 20, 1, new Loot('Gossip and Insults', 'attack', 0), new Loot('Cheer Suit', 'hp', 0), 5);
+  let enemyArray = [jock, avClub, cheer];
+  constructor(player) {
+    this.player = player;
+    this.enemies = enemyArray[Math.floor(Math.random() * 3)];
+    this.turn = whoStarts();
   }
-  if (failureLog.length > 0) {
-    return failureLog;
-  } else {
-    return "PASS";
-  }
-}
 
-Game.prototype.noDupe = function() {
-  var failureLog = [];
-  var keepLog = [];
-  var dumbLog = [];
-  for (var i = 0; i < 9; i++) {
-    var num = this.row1[i]
-    if (num === "") {
-    dumbLog.push("");
-    } else if (keepLog.indexOf(num) < 0) {
-      keepLog.push(this.row1[i]);
+  whoStarts() {
+    if (Math.random() < 0.5) {
+      return false;
     } else {
-      failureLog.push(this.row1[num])
+      return true;
     }
   }
-  return failureLog;
 
-}
+  endRound(player, enemy) {
+    if (this.turn) {
+      const enemyLoot = [enemy.weapon, enemy.armor];
+      const playerLoot = [player.weapon, player.armor];
+      playerLoot.forEach(function(item, i) {
+        if (item.stat > enemyLoot[i].stat) {
+          item = enemyLoot[i];
+        }
+      });
+      return "You Win!!!!"
+    } else {
+      return "You Lose"
+    }
+  }
+
+  attack(player, enemy) {
+    if (this.turn) {
+      let totalAttack = player.baseAttack + player.weapon.stat - enemy.armor.stat;
+      if (enemy.hp - totalAttack <= 0) {
+        this.endRound(player, enemy);
+      } else {
+        enemy.hp = enemy.hp - totalAttack;
+        this.turn = !(this.turn);
+        setTimeout(function() {
+          attack(player, enemy)
+        }, 2000);
+      }
+    } else {
+      let totalAttack = enemy.baseAttack + enemy.weapon.stat;
+      if (player.hp - totalAttack <= 0) {
+        this.endRound(player, enemy);
+      } else {
+        player.hp = player.hp - totalAttack;
+        this.turn = !(this.turn);
+        setTimeout(function() {
+          attack(player, enemy)
+        }, 2000);
+      }
+
+    }
+  }
+
+  export class Character {
+    constructor(name, level, hp, attack, weapon, armor, brains) {
+      this.name = name;
+      this.level = level;
+      this.brains = brains;
+      this.hp = hp;
+      this.baseAttack = attack;
+      this.weapon = weapon;
+      this.armor = armor;
+
+      this.player = false;
+    }
+
+    selectCharacter() {
+      this.player = true;
+    }
+
+    useStuff(loot) {
+      this[loot.type] += loot.stat;
+    }
+
+  }
+
+  export class Loot {
+    constructor(name, type, stat) {
+      this.name = name;
+      this.type = type;
+      this.stat = stat;
+    }
+  }
